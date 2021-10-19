da400a9ed1d9bb8548515dee49acd1682fce2a14
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-10-19 14:15:02 -0300

    create class beneficios

--- src/main/java/com/marcoswinicios/jobs/entidades/Empresa.java
+++ src/main/java/com/marcoswinicios/jobs/entidades/Empresa.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -27,6 +28,8 @@ public class Empresa {
 	private Long id;
 	
 	private String nome;
+	
+	@Lob
 	private String descricao;
 	private String link;
 	
